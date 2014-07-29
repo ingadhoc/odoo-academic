fcb1417213d4601ce1ec69bcc5e70cae7548fc8f
Juan Jose Scarafia <scarafia.juanjose@gmail.com>
2014-07-29 12:57:48 -0300

    REM o2m fields on partner that where not used and gives error

--- addons/academic_x/res_partner.py
+++ addons/academic_x/res_partner.py
@@ -23,28 +23,19 @@
 
 import re
 from openerp import netsvc
-from openerp.osv import osv, fields
+from openerp.osv import osv
+# from openerp.osv import osv, fields
+from openerp import fields
 
 class partner(osv.osv):
     """"""
     
     _inherit = 'res.partner'
 
-    _columns = {
-        # We rewrite partnert_type  to add  change_default=True,
-        'partner_type': fields.selection([(u'student', u'Student'), (u'teacher', u'Teacher'), (u'administrator', u'Administrator'),], string='partner_type', change_default=True,),
-        'teacher_ids': fields.one2many('res.partner','company_id', string='Teachers', domain=[('partner_type','=','teacher')], context={'default_partner_type':'teacher'},),
-        'student_ids': fields.one2many('res.partner','company_id', string='Students', domain=[('partner_type','=','student')], context={'default_partner_type':'student'},),
-        'administrator_ids': fields.one2many('res.partner','company_id', string='Administrators', domain=[('partner_type','=','administrator')], context={'default_partner_type':'administrator'},),
-    }
-
-    _defaults = {
-    }
-
-
-    _constraints = [
-    ]
-
-
+    partner_type = fields.Selection([(u'student', u'Student'), (u'teacher', u'Teacher'), (u'administrator', u'Administrator'),], string='partner_type', change_default=True,)
+    # Estos tres campos no existen mas, si se quieren tendria que ir en res.company
+    # administrator_ids = fields.One2many('res.partner','company_id', string='Administrators', domain=[('partner_type','=','administrator')], context={'default_partner_type':'administrator'},)
+    # teacher_ids = fields.One2many('res.partner','company_id', string='Teachers', domain=[('partner_type','=','teacher')], context={'default_partner_type':'teacher'},)
+    # student_ids = fields.One2many('res.partner','company_id', string='Students', domain=[('partner_type','=','student')], context={'default_partner_type':'student'},)
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
