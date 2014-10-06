cc8cd64fa3d74991c3fc3d7a9cbc59aa81945e16
Juan Jose Scarafia <scarafia.juanjose@gmail.com>
2014-10-06 19:46:22 -0300

    FIR for auth crypt on odoo v8

--- addons/academic_fixes_and_customizations/__openerp__.py
+++ addons/academic_fixes_and_customizations/__openerp__.py
@@ -28,7 +28,7 @@
     'category': 'Tools',
     'demo_xml': [
         ],
-    'depends': ['base','survey','academic_x','web'],
+    'depends': ['base','survey','academic_x','web','auth_crypt'],
     'description': 'Academic Fixes and Customizations',
     'init_xml': [],
     'license': 'AGPL-3',
