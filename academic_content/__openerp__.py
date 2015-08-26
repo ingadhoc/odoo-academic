19cd88ee6236dd383090589ccb958f66d614f7b9
Juan Jose Scarafia <jjs@adhoc.com.ar>
2015-08-26 11:43:27 -0300

    IMP add academic content

---
+++ academic_content/__openerp__.py
@@ -0,0 +1,25 @@
+{'active': False,
+    'author': 'No author.',
+    'category': u'base.module_category_knowledge_management',
+    'data': [
+                'view/project_view.xml',
+                'view/academic_menuitem.xml',
+                'view/res_company.xml',
+                'view/website_view.xml',
+                'security/security.xml',
+                'security/ir.model.access.csv',
+
+    ],
+    'depends': [
+        'academic',
+        'website_security',
+    ],
+    'demo': [],
+    'description': u'Academic Content',
+    'installable': True,
+    'license': 'AGPL-3',
+    'name': u'Academic Content',
+    'test': [],
+    'version': 'No version',
+    'website': ''}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ academic_content/website.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from openerp import fields, models
+
+
+class website_menu(models.Model):
+    _inherit = "website.menu"
+
+    academic_project_id = fields.Many2one(
+        'academic.project',
+        string='Project',
+        )
