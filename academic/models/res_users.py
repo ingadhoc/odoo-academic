faf06b76ce3a03b60303e21f6fb79e442cb41771
Juan Jose Scarafia <jjs@adhoc.com.ar>
2019-01-15 12:24:19 -0300

    [FIX]

--- academic/models/res_users.py
+++ academic/models/res_users.py
@@ -8,21 +8,21 @@ from odoo import models, fields
 class ResUsers(models.Model):
     _inherit = 'res.users'
 
-    """ Con esto hacemos que las passwords no se encripten asi se pueden
-    imprimir en el reporte"""
-    password = fields.Char(
-        'Password',
-        size=64,
-        invisible=True,
-        copy=False,
-        help="Keep empty if you don't want the user to be able"
-        " to connect on the system.",
-    )
+    # """ Con esto hacemos que las passwords no se encripten asi se pueden
+    # imprimir en el reporte"""
+    # password = fields.Char(
+    #     'Password',
+    #     size=64,
+    #     invisible=True,
+    #     copy=False,
+    #     help="Keep empty if you don't want the user to be able"
+    #     " to connect on the system.",
+    # )
 
-    def _set_encrypted_password(self, encrypted):
-        """ Store the provided encrypted password to the database, and clears
-        any plaintext password
+    # def _set_encrypted_password(self, encrypted):
+    #     """ Store the provided encrypted password to the database, and clears
+    #     any plaintext password
 
-        :param uid: id of the current user
-        :param id: id of the user on which the password should be set
-        """
+    #     :param uid: id of the current user
+    #     :param id: id of the user on which the password should be set
+    #     """
