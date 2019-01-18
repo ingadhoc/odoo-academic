9cc99a2bd802e0ef67f0413602095f358f9b9532
Katherine Zaoral <kz@adhoc.com.ar>
2019-01-18 12:33:42 -0300

    [pylint] academic_content: Fix pylints errors

--- academic_content/__manifest__.py
+++ academic_content/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 ##############################################################################
 #
 #    Copyright (C) 2015  ADHOC SA  (http://www.adhoc.com.ar)
@@ -36,7 +35,6 @@
         'website_security',
     ],
     'demo': [],
-    'description': 'Academic Content',
     'installable': True,
     'license': 'AGPL-3',
     'name': 'Academic Content',
