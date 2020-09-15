3d4544a1e87a90025b001849f5bd42f6b878bd94
nmadhoc <nm@adhoc.com.ar>
2020-09-15 14:53:14 -0300

    [MIG] Migration to v13. Added academic.study.plan and hr.employee.asignatures. Deleted academic.group_evaluation

--- academic/models/__init__.py
+++ academic/models/__init__.py
@@ -8,7 +8,16 @@ from . import academic_division
 from . import academic_group
 from . import academic_group_evaluation
 from . import academic_level
+from . import academic_period
+from . import academic_study_plan
 from . import res_users
 from . import academic_promotion
 from . import academic_section
 from . import academic_subject
+from . import survey_question
+from . import survey_question_indicator
+from . import survey_cuestion_variable
+from . import survey_question_performance
+from . import survey_user_input_question_score
+from . import res_company
+from . import hr
