f561ba92dfb0ac87030598c9df08e1d5273a94d8
Juan José Scarafia <jjs@adhoc.com.ar>
2023-10-29 11:23:32 +0000

    [WIP]
    Part-of: ingadhoc/odoo-academic#95

--- academic/models/__init__.py
+++ academic/models/__init__.py
@@ -14,3 +14,6 @@ from . import res_company
 from . import res_partner_relationship
 from . import res_partner
 from . import res_users
+from . import res_partner_role
+from . import res_partner_link
+from . import sale_order
