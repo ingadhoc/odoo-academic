40ec523aff72b52f1ccc6776f9e50fcf572dcaed
Franco Leyes <lef@adhoc.com.ar>
2024-07-08 13:10:37 -0300

    [ADD] academic_split_name: add new module

---
+++ academic_split_name/__manifest__.py
@@ -0,0 +1,20 @@
+# © 2016 ADHOC SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': 'Academic Split Name',
+    'version': "16.0.1.0.0",
+    'sequence': 14,
+    'summary': '',
+    'author': 'ADHOC SA',
+    'website': 'www.adhoc.com.ar',
+    'license': 'AGPL-3',
+    'depends': [
+        'academic',
+    ],
+    'data': [
+        'views/res_partner_views.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
