a7fc1acc3107fa5f8de278aad491e9be517cebed
Juan José Scarafia <jjs@adhoc.com.ar>
2024-11-08 16:39:54 -0300

    [IMP] academic: send sales emails to partner invoice

--- academic/models/account_move.py
+++ academic/models/account_move.py
@@ -28,3 +28,25 @@ class AccountMove(models.Model):
                 rec.student_ids = [(6, 0, student_ids)]
             else:
                 rec.student_ids = [(5, 0, 0)]
+
+    def _message_get_default_recipients(self):
+        """ Por defecto las plantillas mandan a partner_id pero para nosotros el partners es el estudiante.
+        Cambiamos plantillas para que usen el campo "use_default_to" y luego cae en este método de python donde
+        podemos ir mejorando a medida que nos pidan y modificar la logica de recipients.
+        Por ahora lo mandamos solo al partner de facturación si está definido
+        En facturas puntulamente, odoo suscribe al parter id en el metodo post.
+        Dejamos ese feateure y decidimos agregar otros responsables de pago en el envio de email
+        """
+        default_recipients = super()._message_get_default_recipients()
+        for record in self:
+            # calculo analogo sale.order.partner_invoice_ids (Eventulamente podemos hacer un helper en student link)
+            partners_invoice = record.student_id.student_link_ids.filtered(
+                lambda x: self.env.ref('academic.paying_role') in x.role_ids).mapped('partner_id') if record.student_id else False
+            payment_responsible = record.partner_id | partners_invoice
+            if payment_responsible:
+                default_recipients[record.id] = {
+                    'email_cc': False,
+                    'email_to': False,
+                    'partner_ids': payment_responsible.ids,
+                }
+        return default_recipients
