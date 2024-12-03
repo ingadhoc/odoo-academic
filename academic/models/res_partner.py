2233b53aedbb08d6886902f1ab5423db8c8879eb
Franco Leyes <lef@adhoc.com.ar>
2024-12-03 15:59:57 +0000

    [IMP] academic: multi-company partner category
    closes ingadhoc/odoo-academic#140
    
    Signed-off-by: Juan José Scarafía <jjs@adhoc.com.ar>

--- academic/models/res_partner.py
+++ academic/models/res_partner.py
@@ -106,6 +106,7 @@ class ResPartner(models.Model):
     # is_family = fields.Boolean()
     same_dni_partner_id = fields.Many2one('res.partner', string='Partner with same DNI', compute='_compute_same_dni_partner_id', store=False)
     current_main_group_id = fields.Many2one('academic.group', compute='_compute_current_main_group', store=True)
+    category_id = fields.Many2many(check_company=True)
 
     # @api.depends('is_family')
     # def _compute_company_type(self):
