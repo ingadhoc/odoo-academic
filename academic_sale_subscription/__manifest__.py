c71081a558a0c620449af7c878fd28ea9a4d4edf
Franco Leyes <lef@adhoc.com.ar>
2024-12-11 15:25:41 +0000

    [FIX] academic_sale_subscription: change of manifest
    X-original-commit: 3b6aa52a1edd2b1769e36e24f9681506f582e778

--- academic_sale_subscription/__manifest__.py
+++ academic_sale_subscription/__manifest__.py
@@ -2,7 +2,13 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'Academic Sale Subscription',
+<<<<<<< HEAD
     'version': "18.0.1.0.0",
+||||||| parent of 6923882 (temp)
+    'version': "17.0.1.1.0",
+=======
+    'version': "17.0.1.2.0",
+>>>>>>> 6923882 (temp)
     'sequence': 14,
     'summary': '',
     'author': 'ADHOC SA',
