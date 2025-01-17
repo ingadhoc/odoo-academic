06794f0900ea318d1f13408af5c0ef4b398b9648
Rocío Vega <rov@adhoc.com.ar>
2025-01-17 19:58:07 +0000

    [FIX] account_interests: Fix when having interval to run interest
    closes ingadhoc/odoo-academic#189
    
    X-original-commit: 04cdd732f725031593642278f0d8d1fe10696953
    Related: ingadhoc/account-financial-tools#654
    Signed-off-by: Ignacio Cainelli <ica@adhoc.com.ar>
    Signed-off-by: rov-adhoc <rov@adhoc.com.ar>

--- academic_account_interests/__manifest__.py
+++ academic_account_interests/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Academic Account Interests',
-    'version': "18.0.1.0.0",
+    'version': "18.0.1.1.0",
     'sequence': 14,
     'summary': '',
     'author': 'ADHOC SA',
