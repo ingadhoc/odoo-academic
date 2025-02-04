349dbeceab41984653a55c1d478c1cb65ff53286
Franco Leyes <lef@adhoc.com.ar>
2025-02-04 20:04:33 +0000

    [IMP] academic_sale_subscription: add button to create order in group
    closes ingadhoc/odoo-academic#202
    
    Signed-off-by: matiasperalta1 <mnp@adhoc.com.ar>

---
+++ academic_sale_subscription/models/academic_group.py
@@ -0,0 +1,16 @@
+##############################################################################
+# For copyright and license notices, see __manifest__.py file in module root
+# directory
+##############################################################################
+from odoo import models
+
+
+class AcademicGroup(models.Model):
+    _inherit = 'academic.group'
+
+    def open_order_wizard(self):
+        action = self.env.ref('academic_sale_subscription.action_view_academic_order_wizard').read()[0]
+        action.update({
+            'context': {'default_student_ids': self.student_ids.ids}
+        })
+        return action
