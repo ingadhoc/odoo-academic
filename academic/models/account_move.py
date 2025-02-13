3e95b99eeb7ff59b1a4be7fdc2bb8208c3bf4bcb
Franco Leyes <lef@adhoc.com.ar>
2025-02-13 08:53:12 -0300

    [IMP] academic: boolean to require student in invoices

--- academic/models/account_move.py
+++ academic/models/account_move.py
@@ -8,15 +8,17 @@ class AccountMove(models.Model):
     # Este campo solo lo uso para calcular el dominio del student_id ya que implica una búsqueda por el rol de pago.
     student_ids = fields.Many2many('res.partner', string="Students List", compute="_compute_student_ids")
     student_id = fields.Many2one('res.partner', domain="[('id', 'in', student_ids), ('partner_type', '=', 'student')]", index=True)
+    require_student_on_invoices = fields.Boolean(related='company_id.require_student_on_invoices')
 
     @api.constrains('student_id', 'move_type')
     def _check_student(self):
-        invoices_wo_student = self.filtered(lambda x: x.move_type in ["out_invoice", "out_refund"] and not x.student_id)
-        if invoices_wo_student:
-            msg = _("Las facturas de clientes y notas de debito debe tener asociado siempre un alumno.")
-            if len(invoices_wo_student) > 1:
-                msg += ".\n" + _("Los siguientes documentos no cumplen esa condición:") + "\n\n - %s" % '\n - '.join(invoices_wo_student.mapped('display_name'))
-            raise ValidationError(msg)
+        if self.filtered('require_student_on_invoices'):
+            invoices_wo_student = self.filtered(lambda x: x.move_type in ["out_invoice", "out_refund"] and not x.student_id)
+            if invoices_wo_student:
+                msg = _("Las facturas de clientes y notas de debito debe tener asociado siempre un alumno.")
+                if len(invoices_wo_student) > 1:
+                    msg += ".\n" + _("Los siguientes documentos no cumplen esa condición:") + "\n\n - %s" % '\n - '.join(invoices_wo_student.mapped('display_name'))
+                raise ValidationError(msg)
 
     @api.depends('partner_id')
     def _compute_student_ids(self):
--- academic/models/res_company.py
+++ academic/models/res_company.py
@@ -2,7 +2,8 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
-from odoo import models, fields
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError
 
 
 class ResCompany(models.Model):
@@ -19,3 +20,18 @@ class ResCompany(models.Model):
         string='Plan de Estudio'
     )
     family_required = fields.Boolean()
+    require_student_on_invoices = fields.Boolean(default=True)
+
+    @api.constrains('require_student_on_invoices')
+    def _check_invoices_with_student(self):
+        cia_require_students = self.filtered('require_student_on_invoices')
+        if cia_require_students:
+            invoices = self.env['account.move'].search([
+                ('company_id', 'in', cia_require_students.ids),
+                ('move_type', 'in', ["out_invoice", "out_refund"]),
+                ('student_id', '=', False)
+            ], limit=1)
+            if invoices:
+                raise UserError(
+                    _('You cannot mark require student on invoices if there are already invoices without students.')
+                )
