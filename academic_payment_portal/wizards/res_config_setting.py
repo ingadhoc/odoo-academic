8cb7892f7ed999fb122c7e2d791cb32ca243cfec
Martin Quinteros <maq@adhoc.com.ar>
2025-02-24 18:48:32 +0000

    [IMP] New module academic_payment_portal
    If active, portal users will only see the pay button on the oldest due date invoice
    
    closes ingadhoc/odoo-academic#219
    
    Signed-off-by: lav-adhoc <lav@adhoc.com.ar>

---
+++ academic_payment_portal/wizards/res_config_setting.py
@@ -0,0 +1,9 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import fields, models
+
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = 'res.config.settings'
+
+    users_can_pay_only_oldest_invoice = fields.Boolean(related="company_id.users_can_pay_only_oldest_invoice", readonly=False)
