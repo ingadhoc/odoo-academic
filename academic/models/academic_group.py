a3bdc1f7f8a9ada2fe1ef41a136c997debb8f29b
Franco Leyes <lef@adhoc.com.ar>
2025-03-25 14:43:22 +0000

    [IMP] academic: enrollment development

--- academic/models/academic_group.py
+++ academic/models/academic_group.py
@@ -2,11 +2,10 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
-import random
-import string
 from datetime import date
 
-from odoo import api, fields, models
+from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
 
 
 class AcademicGroup(models.Model):
@@ -43,11 +42,19 @@ class AcademicGroup(models.Model):
         context={"default_is_company": True},
         default=lambda self: self.env.company,
     )
-    study_plan_level_ids = fields.Many2many(related="company_id.study_plan_id.level_ids")
+    section_ids = fields.Many2many("academic.section", related="company_id.section_ids")
+    section_id = fields.Many2one(
+        "academic.section",
+        string="Study Plan",
+        required=True,
+        domain="[('id', 'in', section_ids)]",
+    )
+    level_ids = fields.Many2many(related="section_id.level_ids")
     level_id = fields.Many2one(
         "academic.level",
         string="Level",
         required=True,
+        domain="[('id', 'in', level_ids)]",
     )
     subject_id = fields.Many2one("academic.subject", string="Subject", required=False, index=True)
     teacher_id = fields.Many2one(
@@ -57,6 +64,23 @@ class AcademicGroup(models.Model):
         context={"default_partner_type": "teacher"},
         domain=[("partner_type", "=", "teacher")],
     )
+    fee_so_line_ids = fields.One2many(
+        "sale.order.line",
+        "group_id",
+        string="Main SO Line",
+        domain=[("product_id.academic_product_type", "=", "fee")],
+    )
+    registration_so_line_ids = fields.One2many(
+        "sale.order.line",
+        "group_id",
+        string="Registration SO Line",
+        domain=[("product_id.academic_product_type", "=", "registration")],
+    )
+    opportunities_ids = fields.One2many(
+        "crm.lead",
+        "group_id",
+        string="Opportunities",
+    )
     student_ids = fields.Many2many(
         "res.partner",
         "academic_student_group_ids_student_ids_rel",
@@ -65,54 +89,75 @@ class AcademicGroup(models.Model):
         string="Student",
         context={"default_partner_type": "student"},
         domain=[("partner_type", "=", "student")],
+        compute="_compute_student_ids",
+        store=True,
+        readonly=False,
     )
     name = fields.Char(compute="_compute_name", store=True)
     active = fields.Boolean(default=True)
-    student_ids_count = fields.Integer(
-        string="Student Count",
-        compute="_compute_student_ids_count",
-    )
+    fee_student_count = fields.Integer(compute="_compute_fee_student_count")
+    no_fee_student_count = fields.Integer(compute="_compute_no_fee_student_count")
+    registration_student_count = fields.Integer(compute="_compute_registration_student_count")
+    opportunities_student_count = fields.Integer(compute="_compute_opportunities_student_count")
+    capacity = fields.Integer()
+    vacancies = fields.Integer(compute="_compute_vacancies", store=True)
+    manage_sale_workflow = fields.Boolean(compute="_compute_manage_sale_workflow", store=True, readonly=False)
 
     @api.depends("company_id", "level_id", "division_id", "year")
     def _compute_name(self):
         for line in self:
             name_parts = [
                 line.company_id.name,
-                line.level_id.name if line.level_id else None,
+                line.section_id.name,
+                line.level_id.name,
                 line.division_id.name if line.division_id else None,
-                line.level_id.section_id.name if line.level_id and line.level_id.section_id else None,
                 self.env._("Year: %s", line.year),
             ]
             line.name = " - ".join(filter(None, name_parts))
 
-    def create_students_users(self):
-        """
-        This function create users if they don't exist for students related
-         to this group.
-        """
-        self.student_ids.quickly_create_portal_user()
-        # Creamos contrasenas para todos los students que no tengan una
-        # explicita (no hashed)
-        for user in self.student_ids.mapped("user_ids").filtered(lambda x: not x.password):
-            user.password = "".join(random.choice(string.ascii_uppercase + string.digits) for _ in range(6))
-
-    def print_users(self):
-        """
-        This function prints a report with users login and password.
-        """
-        self.ensure_one()
-        self.create_students_users()
-        report = (
-            self.env["ir.actions.report"]
-            .search([("report_name", "=", "academic.template_report_users")], limit=1)
-            .report_action(self)
-        )
-        return report
-
-    @api.depends("student_ids")
-    def _compute_student_ids_count(self):
+    def _compute_fee_student_count(self):
         for group in self:
-            group.student_ids_count = len(group.student_ids)
+            group.fee_student_count = len(
+                group.fee_so_line_ids.filtered(lambda x: x.state == "sale").mapped("order_id.partner_id")
+            )
+
+    def _compute_no_fee_student_count(self):
+        for group in self:
+            group.no_fee_student_count = len(
+                group.registration_so_line_ids.filtered(lambda x: x.state in ["sale"]).mapped("order_id.partner_id")
+                - group.fee_so_line_ids.mapped("order_id.partner_id")
+            )
+
+    def _compute_registration_student_count(self):
+        for group in self:
+            group.registration_student_count = len(
+                group.registration_so_line_ids.filtered(lambda x: x.state in ["draft", "sent"]).mapped(
+                    "order_id.partner_id"
+                )
+            )
+
+    def _compute_opportunities_student_count(self):
+        for group in self:
+            group.opportunities_student_count = len(
+                group.opportunities_ids.filtered(
+                    lambda x: x.active
+                    and x.partner_id
+                    not in group.registration_so_line_ids.mapped("order_id.partner_id")
+                    | group.fee_so_line_ids.mapped("order_id.partner_id")
+                )
+            )
+
+    @api.depends("fee_so_line_ids.order_id.state", "capacity", "manage_sale_workflow")
+    def _compute_vacancies(self):
+        for group in self:
+            if group.manage_sale_workflow:
+                group.vacancies = group.capacity - len(
+                    group.registration_so_line_ids.filtered(lambda x: x.order_id.state == "sale").mapped(
+                        "order_id.partner_id"
+                    )
+                )
+            else:
+                group.vacancies = group.capacity - len(group.student_ids)
 
     def create_next_year_groups(self):
         # estamos pasando de un año a otro sin usar study plan por lo siguiente:
@@ -148,3 +193,94 @@ class AcademicGroup(models.Model):
             }
         )
         return action
+
+    @api.constrains("vacancies")
+    def _check_vacancies(self):
+        if self.filtered(lambda x: x.vacancies < 0):
+            raise ValidationError(_("There can be no negative vacancies. Increase group capacity."))
+
+    def open_opportunities(self):
+        action = self.env["ir.actions.actions"]._for_xml_id("crm.crm_lead_action_pipeline")
+        action.update(
+            {
+                "context": {"search_default_group_id": self.id},
+            }
+        )
+        return action
+
+    def open_registration_sales(self):
+        action = self.env["ir.actions.actions"]._for_xml_id("sale.action_quotations_with_onboarding")
+        action.update({"domain": [("id", "in", self.registration_so_line_ids.mapped("order_id").ids)], "context": {}})
+        return action
+
+    def open_no_fee_students(self):
+        action = self.env.ref("academic.action_academic_partner_students").read()[0]
+        action.update(
+            {
+                "domain": [
+                    (
+                        "id",
+                        "in",
+                        (
+                            self.registration_so_line_ids.filtered(lambda x: x.state in ["sale"]).mapped(
+                                "order_id.partner_id"
+                            )
+                            - self.fee_so_line_ids.mapped("order_id.partner_id")
+                        ).ids,
+                    )
+                ],
+                "views": [(False, "list"), (False, "form")],
+                "context": {"from_open_student_view": True},
+            }
+        )
+        return action
+
+    def open_fee_sales(self):
+        action = self.env["ir.actions.actions"]._for_xml_id("sale.action_quotations_with_onboarding")
+        action.update(
+            {
+                "domain": [
+                    ("id", "in", self.fee_so_line_ids.filtered(lambda x: x.state == "sale").mapped("order_id").ids)
+                ],
+                "context": {},
+            }
+        )
+        return action
+
+    def open_students(self):
+        action = self.env.ref("academic.action_academic_partner_students").read()[0]
+        action.update(
+            {
+                "domain": [("id", "in", self.student_ids.ids)],
+                "views": [(False, "list"), (False, "form")],
+                "context": {"from_open_student_view": True},
+            }
+        )
+        return action
+
+    @api.depends("manage_sale_workflow", "registration_so_line_ids.state", "fee_so_line_ids.state")
+    def _compute_student_ids(self):
+        for group in self.filtered("manage_sale_workflow"):
+            group.student_ids = group.fee_so_line_ids.filtered(lambda x: x.order_id.state == "sale").mapped(
+                "order_id.partner_id"
+            ) | group.registration_so_line_ids.filtered(lambda x: x.order_id.state == "sale").mapped(
+                "order_id.partner_id"
+            )
+
+    @api.constrains("capacity")
+    def _check_capacity(self):
+        if self.filtered(lambda x: x.capacity <= 0):
+            raise ValidationError(self.env._("The capacity must be greater than 0."))
+
+    @api.depends("subject_id")
+    def _compute_manage_sale_workflow(self):
+        groups_with_subject = self.filtered("subject_id")
+        groups_with_subject.manage_sale_workflow = False
+        (self - groups_with_subject).manage_sale_workflow = True
+
+    def write(self, vals):
+        if "manage_sale_workflow" in vals and not (
+            self.env.user.has_group("academic.group_manager") or self.env.user._is_admin()
+        ):
+            raise ValidationError(_("You are not allowed to modify the sale workflow setting."))
+        return super().write(vals)
--- academic/models/academic_section.py
+++ academic/models/academic_section.py
@@ -8,7 +8,25 @@ from odoo import fields, models
 class AcademicSection(models.Model):
     _name = "academic.section"
     _description = "section"
+    _order = "sequence"
 
     name = fields.Char(
         required=True,
+    )
+    correlative_ids = fields.Many2many(
+        "academic.section",
+        "academic_section_correlative_ids_rel",
+        "section_id",
+        "correlative_id",
+        string="Correlative Study Plans",
+    )
+    level_ids = fields.Many2many(
+        "academic.level",
+        "academic_section_level_ids_rel",
+        "section_id",
+        "level_id",
+        string="Levels",
+    )
+    sequence = fields.Integer(
+        default=10,
     )
