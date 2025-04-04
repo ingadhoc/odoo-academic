f6f6e9432efffb8262ea5a5608d0cd93dde07a61
Franco Leyes <lef@adhoc.com.ar>
2025-04-04 17:55:56 +0000

    [IMP] academic_sale_subscription: add setting for subscription invoices

--- academic_sale_subscription/models/__init__.py
+++ academic_sale_subscription/models/__init__.py
@@ -5,3 +5,4 @@ from . import sale_subscription_plan
 from . import res_partner
 from . import sale_order_line
 from . import academic_group
+from . import res_config_settings
---
+++ academic_sale_subscription/models/res_config_settings.py
@@ -0,0 +1,10 @@
+from odoo import fields, models
+
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = 'res.config.settings'
+
+    enable_zero_price_subscription_invoice = fields.Boolean(
+        config_parameter='academic_sale_subscription.enable_zero_price_subscription_invoice',
+        string="Invoice subscriptions with zero total",
+    )
