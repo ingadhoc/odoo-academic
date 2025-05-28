d685d54c1241e6052356b67a2c6f7aebe8909d2d
Juan José Scarafia <jjs@adhoc.com.ar>
2025-05-28 14:31:03 -0300

    [REF] academic: move sales and account to academic_sale_subscription

--- academic/models/res_company.py
+++ academic/models/res_company.py
@@ -2,8 +2,7 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
-from odoo import api, fields, models
-from odoo.exceptions import UserError
+from odoo import fields, models
 
 
 class ResCompany(models.Model):
@@ -16,23 +15,3 @@ class ResCompany(models.Model):
     )
     section_ids = fields.Many2many("academic.section", string="Study Plans")
     family_required = fields.Boolean(default=True)  # Falta revisar la implementación con ese booleano en False
-    require_student_on_invoices = fields.Boolean(default=True)
-
-    @api.constrains("require_student_on_invoices")
-    def _check_invoices_with_student(self):
-        cia_require_students = self.filtered("require_student_on_invoices")
-        if cia_require_students:
-            invoices = self.env["account.move"].search(
-                [
-                    ("company_id", "in", cia_require_students.ids),
-                    ("move_type", "in", ["out_invoice", "out_refund"]),
-                    ("student_id", "=", False),
-                ],
-                limit=1,
-            )
-            if invoices:
-                raise UserError(
-                    self.env._(
-                        "You cannot mark require student on invoices if there are already invoices without students."
-                    )
-                )
--- academic_sale_subscription/models/sale_order_line.py
+++ academic_sale_subscription/models/sale_order_line.py
@@ -2,7 +2,7 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class SaleOrderLine(models.Model):
@@ -10,6 +10,18 @@ class SaleOrderLine(models.Model):
 
     partner_invoice_id = fields.Many2one(related="order_id.partner_invoice_id")
     payment_term_id = fields.Many2one(related="order_id.payment_term_id")
+    group_id = fields.Many2one("academic.group", compute="_compute_group_id", store=True, readonly=False)
+    academic_product_type = fields.Selection(
+        related="product_id.academic_product_type",
+    )
+
+    @api.depends("product_id")
+    def _compute_group_id(self):
+        for rec in self:
+            if rec.product_id.academic_product_type and rec.order_id.opportunity_id.group_id:
+                rec.group_id = rec.order_id.opportunity_id.group_id
+            else:
+                rec.group_id = False
 
     def open_subscription_form(self):
         return {
