baed82cff924e803afde5baf288f36387f3d1310
Juan José Scarafia <jjs@adhoc.com.ar>
2025-05-28 17:31:03 +0000

    [REF] academic: move sales and account to academic_sale_subscription
    Part-of: ingadhoc/odoo-academic#272

--- academic_sale_subscription/__init__.py
+++ academic_sale_subscription/__init__.py
@@ -3,3 +3,4 @@
 from . import models
 from . import wizard
 from . import controllers
+from .hooks import post_init_hook
--- academic_sale_subscription/wizard/__init__.py
+++ academic_sale_subscription/wizard/__init__.py
@@ -1,3 +1,4 @@
 # © 2016 ADHOC SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 from . import academic_order_wizard
+from . import account_move_send
