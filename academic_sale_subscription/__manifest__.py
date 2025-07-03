094d045ba87db1581e7c5f1dc4e66fcf39f61e8a
Franco Leyes <lef@adhoc.com.ar>
2025-07-03 14:36:30 +0000

    [IMP] academic_sale_subscription: fix currency display logic for total amount due
    closes ingadhoc/odoo-academic#283
    
    Signed-off-by: matiasperalta1 <mnp@adhoc.com.ar>

--- academic_sale_subscription/__manifest__.py
+++ academic_sale_subscription/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Academic Sale Subscription",
-    "version": "18.0.1.6.0",
+    "version": "18.0.1.7.0",
     "sequence": 14,
     "summary": "",
     "author": "ADHOC SA",
