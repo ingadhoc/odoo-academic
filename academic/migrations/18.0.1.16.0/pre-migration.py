928020236cfa8f226272c871e85091d66d91da45
Franco Leyes <lef@adhoc.com.ar>
2025-07-08 18:42:19 +0000

    [IMP] academic: drop 'academic_identification_number' column if it exists before renaming 'dni' field

--- academic/migrations/18.0.1.16.0/pre-migration.py
+++ academic/migrations/18.0.1.16.0/pre-migration.py
@@ -3,6 +3,15 @@ from openupgradelib import openupgrade
 
 @openupgrade.migrate()
 def migrate(env, version):
+    env.cr.execute("""
+        SELECT column_name
+        FROM information_schema.columns
+        WHERE table_name='res_partner'
+        AND column_name='academic_identification_number'
+    """)
+    if env.cr.fetchone():
+        env.cr.execute("ALTER TABLE res_partner DROP COLUMN academic_identification_number")
+
     openupgrade.rename_fields(
         env,
         [
