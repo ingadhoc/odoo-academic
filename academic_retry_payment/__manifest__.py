7b55e643843d827f04946075169dae8e224ca8d1
Martin Quinteros <maq@adhoc.com.ar>
2025-07-31 15:57:23 -0300

    [ADD] Add new bridge module academic_retry_payment

---
+++ academic_retry_payment/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Academic retry Payment",
+    "version": "18.0.1.0.0",
+    "sequence": 14,
+    "summary": "",
+    "author": "ADHOC SA",
+    "website": "www.adhoc.com.ar",
+    "license": "AGPL-3",
+    "depends": ["academic_sale_subscription", "payment_retry"],
+    "data": [
+        "wizards/payment_transaction_retry.xml",
+    ],
+    "installable": True,
+    "auto_install": True,
+    "application": False,
+}
