047047b683076bc41a3a64944774b0de9189b263
Franco Leyes <lef@adhoc.com.ar>
2025-09-26 16:27:39 -0300

    [FIX] spreadsheet_dashboard_academic: adjustments in json
    closes ingadhoc/odoo-academic#308
    
    Signed-off-by: matiasperalta1 <mnp@adhoc.com.ar>

--- spreadsheet_dashboard_academic/__manifest__.py
+++ spreadsheet_dashboard_academic/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'Spreadsheet Dashboard Academic',
-    'version': "17.0.1.0.0",
+    'version': "17.0.1.1.0",
     'sequence': 14,
     'summary': 'Academic Spreadsheet Dashboard for Invoice Tracking',
     'author': 'ADHOC SA',
