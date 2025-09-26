6ed439d522c8645b5c330a2d19f55b10c4739875
Franco Leyes <lef@adhoc.com.ar>
2025-09-26 19:27:39 +0000

    [FIX] spreadsheet_dashboard_academic: adjustments in json
    X-original-commit: 047047b683076bc41a3a64944774b0de9189b263

--- spreadsheet_dashboard_academic/__manifest__.py
+++ spreadsheet_dashboard_academic/__manifest__.py
@@ -1,6 +1,7 @@
 # © 2016 ADHOC SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
+<<<<<<< 6aabafedb4ef4970a6d8d87b273a5d4dd81207f1
     "name": "Spreadsheet Dashboard Academic",
     "version": "18.0.1.0.0",
     "sequence": 14,
@@ -12,6 +13,31 @@
     "depends": ["academic_sale_subscription", "spreadsheet_dashboard"],
     "data": [
         "data/dashboards.xml",
+||||||| 5d875681362e8f0bbf9a45ccd23014712174eee3
+    'name': 'Spreadsheet Dashboard Academic',
+    'version': "17.0.1.0.0",
+    'sequence': 14,
+    'summary': 'Academic Spreadsheet Dashboard for Invoice Tracking',
+    'author': 'ADHOC SA',
+    'website': 'www.adhoc.com.ar',
+    'license': 'AGPL-3',
+    'category': 'Academic',
+    'depends': [
+        'academic_sale_subscription',
+        'spreadsheet_dashboard'
+=======
+    'name': 'Spreadsheet Dashboard Academic',
+    'version': "17.0.1.1.0",
+    'sequence': 14,
+    'summary': 'Academic Spreadsheet Dashboard for Invoice Tracking',
+    'author': 'ADHOC SA',
+    'website': 'www.adhoc.com.ar',
+    'license': 'AGPL-3',
+    'category': 'Academic',
+    'depends': [
+        'academic_sale_subscription',
+        'spreadsheet_dashboard'
+>>>>>>> fe6728e6f255147b9cb15392aa201059d683895a
     ],
     "installable": True,
     "auto_install": ["academic_sale_subscription"],
