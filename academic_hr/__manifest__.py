649c68f62ce0be550c3b9cc82427f5bde5b24220
Franco Leyes <lef@adhoc.com.ar>
2025-12-02 20:06:41 +0000

    [IMP] academic_hr: add personal data management to portal with security rules and templates
    X-original-commit: 33114fe0a536def02cf79ee03a0d10b9b988f808

--- academic_hr/__manifest__.py
+++ academic_hr/__manifest__.py
@@ -11,12 +11,16 @@
         "hr_holidays",
         "hr_timesheet",
         "timesheet_grid",
+        "portal",
     ],
     "data": [
+        "security/academic_hr_security.xml",
+        "security/ir.model.access.csv",
         "views/hr_leave_views.xml",
         "views/hr_timesheet_views.xml",
         "views/hr_employee_views.xml",
         "views/hr_leave_allocation_views.xml",
+        "views/portal_templates.xml",
     ],
     "assets": {
         "web.assets_backend": [
