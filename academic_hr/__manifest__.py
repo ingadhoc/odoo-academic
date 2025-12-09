7bbaaf182ec466f531db178cf1ff7de329865003
Franco Leyes <lef@adhoc.com.ar>
2025-12-09 12:42:46 -0300

    [IMP] academic_hr: add web_ribbon in employee
    closes ingadhoc/odoo-academic#345
    
    Signed-off-by: matiasperalta1 <mnp@adhoc.com.ar>

--- academic_hr/__manifest__.py
+++ academic_hr/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Academic HR",
-    "version": "18.0.1.0.0",
+    "version": "18.0.1.1.0",
     "category": "Human Resources",
     "summary": "HR extensions for academic institutions with multiple employee support",
     "author": "ADHOC SA",
@@ -18,6 +18,11 @@
         "views/hr_employee_views.xml",
         "views/hr_leave_allocation_views.xml",
     ],
+    "assets": {
+        "web.assets_backend": [
+            "academic_hr/static/src/css/ribbon.css",
+        ],
+    },
     "installable": True,
     "auto_install": False,
     "application": False,
