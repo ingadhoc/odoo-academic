6506d2830ab34245e8e9992e3d322f205bdcc118
Franco Leyes <lef@adhoc.com.ar>
2026-03-31 11:05:05 -0300

    [REF] academic_sale_subscription: Remove payment_transaction model and its import from __init__.py
    closes ingadhoc/odoo-academic#392
    
    Signed-off-by: lav-adhoc <lav@adhoc.com.ar>

--- academic_sale_subscription/models/payment_transaction.py
+++
@@ -1,8 +0,0 @@
-from odoo import models
-
-
-class PaymentTransaction(models.Model):
-    _inherit = "payment.transaction"
-
-    def _post_process(self):
-        return super(PaymentTransaction, self.with_context(from_payment_processing=True))._post_process()
