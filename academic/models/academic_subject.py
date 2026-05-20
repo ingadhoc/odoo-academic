772b2a4174830b8c38821e6f6c429357e8ad08db
Franco Leyes <lef@adhoc.com.ar>
2026-05-20 20:58:21 +0000

    [IMP] academic: introduce subject templates and enhance subject management with state and company constraints
    closes ingadhoc/odoo-academic#411
    
    Signed-off-by: matiasperalta1 <mnp@adhoc.com.ar>

--- academic/models/academic_subject.py
+++ academic/models/academic_subject.py
@@ -2,15 +2,47 @@
 # For copyright and license notices, see __manifest__.py file in module root
 # directory
 ##############################################################################
-from odoo import fields, models
+from odoo import api, fields, models
+from odoo.exceptions import ValidationError
 
 
 class AcademicSubject(models.Model):
     _name = "academic.subject"
     _description = "subject"
+    _sql_constraints = [
+        (
+            "academic_subject_template_state_company_unique",
+            "unique(template_id, state_id, company_id)",
+            "There is already a subject for this template, jurisdiction and company.",
+        ),
+    ]
 
     name = fields.Char(
         required=True,
+    )
+    company_id = fields.Many2one(
+        "res.company",
+        required=True,
+        default=lambda self: self.env.company,
+        index=True,
+    )
+    state_id = fields.Many2one(
+        "res.country.state",
+        related="company_id.state_id",
+        index=True,
+        readonly=True,
+        store=True,
+    )
+    template_id = fields.Many2one(
+        "academic.subject.template",
+        required=True,
+        ondelete="restrict",
+        index=True,
+    )
+    code = fields.Char(
+        related="template_id.code",
+        readonly=True,
+        store=True,
     )
     group_ids = fields.One2many(
         "academic.group",
@@ -18,3 +50,9 @@ class AcademicSubject(models.Model):
         string="Groups",
     )
     employees_asignatures_ids = fields.One2many(comodel_name="hr.employee.asignatures", inverse_name="subject_id")
+
+    @api.constrains("company_id", "state_id")
+    def _check_company_state_consistency(self):
+        for rec in self:
+            if rec.company_id.state_id and rec.state_id != rec.company_id.state_id:
+                raise ValidationError(self.env._("The subject jurisdiction must match the company jurisdiction."))
--- academic/models/hr.py
+++ academic/models/hr.py
@@ -10,6 +10,16 @@ class EmployeeAsignatures(models.Model):
     name = fields.Char(compute="_compute_name")
 
     teacher_id = fields.Many2one("hr.employee", required=True, ondelete="cascade")
+    teacher_company_id = fields.Many2one(
+        "res.company",
+        related="teacher_id.company_id",
+        readonly=True,
+    )
+    teacher_state_id = fields.Many2one(
+        "res.country.state",
+        related="teacher_company_id.state_id",
+        readonly=True,
+    )
     subject_id = fields.Many2one(
         comodel_name="academic.subject",
     )
@@ -30,18 +40,26 @@ class EmployeeAsignatures(models.Model):
                     ("teacher_id", "=", record.teacher_id.id),
                 ]
             ):
-                raise ValidationError(
-                    self.env._(
-                        "There is another record that contains the same subject \
-                        and teacher"
-                    )
-                )
+                raise ValidationError(self.env._("There is another record that contains the same subject and teacher"))
 
     @api.constrains("subject_id")
     def _check_level_ids(self):
         for record in self:
-            if not record.level_ids:
+            if record.subject_id and not record.level_ids:
                 raise ValidationError(self.env._("You have to add at least a level for that subject"))
+
+    @api.constrains("teacher_id", "subject_id")
+    def _check_subject_scope(self):
+        for record in self:
+            if not record.teacher_id or not record.subject_id:
+                continue
+            employee_company = record.teacher_id.company_id
+            if record.subject_id.company_id and record.subject_id.company_id != employee_company:
+                raise ValidationError(self.env._("The subject company must match the employee company."))
+            if employee_company.state_id and record.subject_id.state_id != employee_company.state_id:
+                raise ValidationError(
+                    self.env._("The subject jurisdiction must match the employee company jurisdiction.")
+                )
 
 
 class Employee(models.Model):
