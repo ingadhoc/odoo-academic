d6ee36283518ddcdffdde88760c6cdec8ecbe519
roboadhoc <roboadhoc@adhoc.com.ar>
2026-06-19 19:08:58 +0000

    [BOT] Bump version: academic_sale_subscription 19.0.1.8.0
    Merged: ingadhoc/odoo-academic#424

--- academic_sale_subscription/__manifest__.py
+++ academic_sale_subscription/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Academic Sale Subscription",
-    "version": "19.0.1.7.0",
+    "version": "19.0.1.8.0",
     "sequence": 14,
     "summary": "",
     "author": "ADHOC SA",
