37f4b08d528695de86e7b2c345ae6af89167f09d
Guido Galetto <gal@adhoc.inc>
2026-07-02 17:28:18 +0000

    [REM] academic: move "grant access to all" button to portal_ux
    The generic "Grant Access to All" button on the portal wizard was
    academic-specific but had no academic logic in it. It now lives in
    the horizontal module portal_ux, available on any database. academic
    depends on portal_ux to keep the button; the partner_type-based group
    assignment override on portal.wizard.user is untouched and keeps
    applying through polymorphism.
    
    closes ingadhoc/odoo-academic#431
    
    Related: ingadhoc/miscellaneous#418
    Signed-off-by: Nicolas Mac Rouillon (ADV) <nmr@adhoc.inc>

--- academic/__manifest__.py
+++ academic/__manifest__.py
@@ -28,6 +28,7 @@
     "images": [],
     "depends": [
         "portal_backend",
+        "portal_ux",
         "hr",
         "website",
         "contacts",
@@ -54,7 +55,6 @@
         "views/res_partner_link_views.xml",
         "views/res_partner_role_views.xml",
         "views/res_partner_relationship_views.xml",
-        "wizards/portal_wizard_views.xml",
         "report/ir_actions_report.xml",
         "report/report_basic_group_data.xml",
         "report/report_certificate_regular_student.xml",
